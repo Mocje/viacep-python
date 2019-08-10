3cab7eedb2fa9e25fe69a08fcdb572a47f3e3f43
Mocje <52362720+Mocje@users.noreply.github.com>
2019-08-10 10:15:37 -0300

    updated main file
    added an "error treatment" to the status code: 200, 401/402 and 404

--- viacep/__init__.py
+++ viacep/__init__.py
@@ -8,14 +8,22 @@ from sys import exit
 __version__ = '1.3.1'
 
 class ViaCEP:
+	self.sit = None
 
-	def __init__(self, cep):
+	#def __init__(self, cep):
+	#	self.cep = cep
+
+	def getDadosCEP(self, cep):
 		self.cep = cep
-
-	def getDadosCEP(self):
 		url_api = ('http://www.viacep.com.br/ws/%s/json' % self.cep)
 		req = requests.get(url_api)
 		if req.status_code == 200:
 			dados_json = json.loads(req.text)
-			return dados_json
-		
+			self.sit = dados_json
+		elif req.status_code == 404:
+			self.sit = {'error': True, '__cause__': 'Not Found', 'error_code': 404}
+			
+		else:
+			self.sit = {'error': True, '__cause__': 'Forbidden' 'error_code': 402}
+			
+		return self.sit
